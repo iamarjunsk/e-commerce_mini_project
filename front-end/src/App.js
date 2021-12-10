04dce6c7a98433ba7868a5d79e3c104bed6128e9
Binusree <binuthottath99@gmail.com>
2021-12-10 20:37:03 +0530

    Home page added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Appbar from './components/Navbar/Appbar'
+import Head from './components/Header/Head'
+import Rowpost from './components/Rowpost/Rowpost';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Appbar />
+        <Head/>
+        <Rowpost/>
+       <Footer/>
     </div>
   );
 }
---
+++ front-end/src/components/Rowpost/Rowpost.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Rowpost.css'
+
+function Rowpost() {
+    return (
+        <div className="rowpost">
+            <div className="head">DEALS OF THE DAY</div>
+            <div className="cont">
+                <div className="row1"> <img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2021/12/9/6dcb27c7-ceee-46ab-8292-f3026a3146f81639019874870-Best-of-styles.jpg" alt="" className="row1" />  </div> 
+                <div className="row1">  <img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2021/12/8/07c252c5-3f28-4ed0-a306-3e127d8d29791638978891180-Winter-Wear.jpg" alt="" className="row1" /></div>                
+                <div className="row1"> <img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2021/12/8/5b98f475-9d26-4e45-8077-3c03e64c704f1638978891173-T-Shirts.jpg" alt="" className="row1"/></div>                
+                <div className="row1"> <img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2021/12/8/312ac963-572a-4c43-99bc-01e7fa90876a1638978891149-Jeans-_-Shirts.jpg" alt="" className="row1"  /></div>                
+                <div className="row1"> <img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2021/12/8/8ab70aca-001f-4ffd-a6eb-bc094b7467211638978891166-Trendiest-Styles.jpg" alt="" className="row1" /> </div>
+            </div>
+            <div className="head">TRENDING</div>
+            <div className="cont">
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/655751e9-66c3-49b5-acfc-a508068e6e581595936000398-Content-westernwear-Trends-Vibrantcolourblocks.jpg" alt="" className="row2"/></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/f7585d6d-78ee-42bd-baf2-07eefada7fb31597840566666-Content-westerncasual-color-moss.jpg" className="row2"/></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/d53a46d7-e8f8-4f7e-9e1a-6cb997d1a1e51597840566613-Content-westerncasual-brand-modarapidoskinnyjeans.jpg" alt="" className="row2" /></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/8116410f-af2c-4980-95ef-04a4162092491597840566722-Content-westerncasual-color-preppyred.jpg" alt="" className="row2" /></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/8/20/5b3b3f9a-d893-4f15-9e1c-c7667bca41091597912691188-Content-westerncasual-trend-superhero.jpg" alt="" className="row2" /></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/655751e9-66c3-49b5-acfc-a508068e6e581595936000398-Content-westernwear-Trends-Vibrantcolourblocks.jpg" alt="" className="row2"/></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/f7585d6d-78ee-42bd-baf2-07eefada7fb31597840566666-Content-westerncasual-color-moss.jpg" className="row2"/></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/d53a46d7-e8f8-4f7e-9e1a-6cb997d1a1e51597840566613-Content-westerncasual-brand-modarapidoskinnyjeans.jpg" alt="" className="row2" /></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/8116410f-af2c-4980-95ef-04a4162092491597840566722-Content-westerncasual-color-preppyred.jpg" alt="" className="row2" /></div>
+            <div className="row2"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/8/20/5b3b3f9a-d893-4f15-9e1c-c7667bca41091597912691188-Content-westerncasual-trend-superhero.jpg" alt="" className="row2" /></div>
+            </div>
+            <div className="head">WOMENS COLLECTION</div>
+            <div className="cont">
+            <div className="row3"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/f0397d0f-9c2a-4c87-956e-9896b615b3061597840342772-Content-ethnicwear-trend-printparadise.jpg" alt="" className="row3" /></div>
+           < div className="row3"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/87b0957c-41f7-4522-9f30-f69087a0dce61597840342593-Content-ethnicwear-color-mustard.jpg" alt="" className="row3" /></div>
+            <div className="row3"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/2ff5c4e9-da10-4d12-9aae-86c38f4c281a1597840342549-Content-ethnicwear-brand-fusiondress.jpg" alt="" className="row3" /></div>
+            <div className="row3"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/8/21/99463248-ae80-47de-836f-3bafe2262bff1598029618274-Content-mostselling-Ethnicwear-Kurtasetsbiba.jpg" alt="" className="row3" /></div>
+            <div className="row3"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/e4fe9b08-9d0a-4349-868f-6ac5aa7123ed1595935030800-Content-ethnicwear-trend-Bsummerreadyethnicdresses.jpg" alt="" className="row3" /></div>
+           </div>
+            <div className="head">MENS CLOTHING</div>
+            <div className="cont">
+            <div className="row4"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/a9f68785-e282-425a-b270-c978c387b0f31597840342635-Content-ethnicwear-color-whites.jpg" alt="" className="row4" /></div>
+            <div className="row4"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/19/0d1e0a28-3088-4719-a692-4cdaa7a33cc71597840342726-Content-ethnicwear-occasion-casuallook.jpg" alt="" className="row4" /></div>
+            <div className="row4"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/9d248917-d1b0-4910-8de0-4ed7c2b4af8e1595935030939-Content-ethnicwear-trends-printedkurtaset.jpg" alt="" className="row4" /></div>
+            <div className="row4"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/83d9ca97-4aa3-46ce-bd28-b135d3b94a021595935030673-Content-ethnicwear-essentials-everydaykurtas.jpg" alt="" className="row4" /></div>
+            <div className="row4"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/7/28/9e4fb95e-6268-49c5-9ed1-e6b1bd4b5efd1595935030880-Content-ethnicwear-trend-fashionmeetcomfort.jpg" alt="" className="row4" /></div>
+            </div>
+            <div className="head">KIDS COLLECTION</div>
+            <div className="cont">
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/9/efc4b19d-179f-4437-961c-839df50299a51604906586690-36-Essentials-Night_innerwear.jpg" alt="" className="row5" /></div>        
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/9/c22c8c4b-753a-4f90-8def-511d2ed6caf31604906586731-37-Essentials-EthnicWear.jpg" alt="" className="row5" /></div>        
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/9/29baf945-9e5b-4f0e-bb05-0ce65c57f9c91604906586502-32-Essentials-Shorts.jpg" alt="" className="row5" /></div>        
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/9/4e6eefd3-3355-4c1c-851e-48a49c97d5b31604906586407-30-Essentials-Onesies.jpg" alt="" className="row5" /></div>
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/9/c75a2039-4199-4174-b1b9-fecd30f7d3f91604906586359-29-Essentials-Tshirts_Tops.jpg" alt="" className="row5" /></div>        
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/9/2ab2f5b3-441a-430c-a605-2ac9d06007c01604906586315-28-Essentials-Dresses.jpg" alt="" className="row5" /></div>
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15666080/2021/9/28/1ace6549-8777-4c02-b84b-328fcf5c74731632836727838Natilene1.jpg" alt="" className="row5" /></div>
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15997584/2021/11/3/8a54858e-8275-43f2-913e-04210cd7056a1635926509101Tracksuits5.jpg" alt="" className="row5" /></div>
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15965476/2021/10/29/9f2d7a6a-c6a8-41df-a82c-c21f2e4fc2791635487505811NautiNatiGirlsBlack1.jpg" alt="" className="row5" /></div>
+            <div className="row5"><img src="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16264412/2021/11/26/b954ea84-f707-42c7-9fe0-b3410e9191491637927953031NautiNatiGirlsBluePrintedRegularPureCottonKurtiwithDhotiPant1.jpg" alt="" className="row5" /></div>
+            </div>
+
+        </div>
+        
+        )
+}
+
+export default Rowpost
