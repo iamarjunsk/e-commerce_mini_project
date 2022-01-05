0137110872a8effcda968cdeadb6f832ba1e44e8
Arjun S K <64605380+iamarjunsk@users.noreply.github.com>
2022-01-05 15:18:01 +0530

    Delete backend directory

--- backend/backend/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from products import views
-
-urlpatterns = [
-    path('latest-products/',views.LatestProductList.as_view()),
-    path('all-category/',views.CategoryList.as_view()),
-]
